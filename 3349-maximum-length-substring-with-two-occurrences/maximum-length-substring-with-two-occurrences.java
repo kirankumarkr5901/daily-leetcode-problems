f23effab56431b5496f19876615cea8999c8369d
Kirankumar K R <74645636+kirankumarkr5901@users.noreply.github.com>
2026-08-14 09:41:59 +0530

    Time: 2 ms (54.50%) | Memory: 45.4 MB (12.03%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
@@ -1,17 +1,34 @@
 class Solution {
     public int maximumLengthSubstring(String s) {
-        int[] count = new int[26];
+        // SLIDING WINDOW
+        // int[] count = new int[26];
+        // int ans = 0;
+        // int n = s.length();
+        // for(int left = 0, right = 0;right < n;right++) {
+        //     int currentIndex = s.charAt(right) - 'a';
+        //     count[currentIndex]++;
+        //     while(count[currentIndex] > 2) {
+        //         int leftIndex = s.charAt(left) - 'a';
+        //         count[leftIndex]--;
+        //         left++;
+        //     }
+        //     ans = Math.max(ans, right - left + 1);
+        // }
+        // return ans;
+
+        // BRUTE FORCE
         int ans = 0;
         int n = s.length();
-        for(int left = 0, right = 0;right < n;right++) {
-            int currentIndex = s.charAt(right) - 'a';
-            count[currentIndex]++;
-            while(count[currentIndex] > 2) {
-                int leftIndex = s.charAt(left) - 'a';
-                count[leftIndex]--;
-                left++;
+        for(int i = 0;i<n;i++) {
+            int[] count = new int[26];
+            for(int j = i;j<n;j++) {
+                int index = s.charAt(j) - 'a';
+                count[index]++;
+                if(count[index] > 2) {
+                    break;
+                }
+                ans = Math.max(ans, j - i + 1);
             }
-            ans = Math.max(ans, right - left + 1);
         }
         return ans;
     }

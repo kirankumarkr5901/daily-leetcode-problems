19792bdfe0fb625695c8dc9c7813549625deaf4a
Kirankumar K R <74645636+kirankumarkr5901@users.noreply.github.com>
2026-08-15 11:23:19 +0530

    Time: 9 ms (64.48%) | Memory: 43 MB (70.31%) - LeetSync

--- 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.java
+++ 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.java
@@ -4,37 +4,57 @@ class Solution {
     }
     public int countVowelSubstrings(String s) {
         int answer = 0;
-        int left = 0;
-        int current = 0;
-        int n = s.length();
+
+        // BRUTE FORCE
         int[] count = new int[26];
-        int uniqueValues = 0;
-        for(int right = 0;right < n;right++) {
-            char c = s.charAt(right);
-            if(isVowel(c)) {
-                int index = c-'a';
-                count[index]++;
-                if(count[index] == 1)
-                    uniqueValues++;
-                
-                while(uniqueValues == 5) {
-                    int currIndex = s.charAt(current) - 'a';
-                    count[currIndex]--;
-                    if(count[currIndex] == 0)
-                        uniqueValues--;
-                    current++;
+        int uniqueValue = 0;
+        int n = s.length();
+        for(int i = 0;i<n;i++) {
+            count = new int[26];
+            uniqueValue = 0;
+            for(int j = i;j<n && isVowel(s.charAt(j));j++) {
+                count[s.charAt(j)-'a']++;
+                if(count[s.charAt(j)-'a'] == 1)
+                    uniqueValue++;
+                if(uniqueValue == 5) {
+                    answer++;
                 }
-
-                answer += (current - 1) - left + 1;
-
-            } else {
-                // reset
-                left = right + 1;
-                current = left;
-                uniqueValues = 0;
-                count = new int[26];
             }
         }
+
+
+        // SLIDING WINDOW
+        // int left = 0;
+        // int current = 0;
+        // int n = s.length();
+        // int[] count = new int[26];
+        // int uniqueValues = 0;
+        // for(int right = 0;right < n;right++) {
+        //     char c = s.charAt(right);
+        //     if(isVowel(c)) {
+        //         int index = c-'a';
+        //         count[index]++;
+        //         if(count[index] == 1)
+        //             uniqueValues++;
+                
+        //         while(uniqueValues == 5) {
+        //             int currIndex = s.charAt(current) - 'a';
+        //             count[currIndex]--;
+        //             if(count[currIndex] == 0)
+        //                 uniqueValues--;
+        //             current++;
+        //         }
+
+        //         answer += (current - 1) - left + 1;
+
+        //     } else {
+        //         // reset
+        //         left = right + 1;
+        //         current = left;
+        //         uniqueValues = 0;
+        //         count = new int[26];
+        //     }
+        // }
         return answer;
     }
 }

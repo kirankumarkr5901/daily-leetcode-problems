ef09bf92b737c43a99449f1c622d55ec47cfe9cb
Kirankumar K R <74645636+kirankumarkr5901@users.noreply.github.com>
2026-08-15 13:11:52 +0530

    Time: 1 ms (96.61%) | Memory: 42.6 MB (56.22%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,13 +1,16 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        int numberOfOne = 0;
-        while(n > 0) {
-            if((n&1) == 1) {
-                numberOfOne++;
-            }
-            n = n >> 1;
-        }
-        // core logic is 2^x will always have single set bit
-        return numberOfOne == 1;
+        // int numberOfOne = 0;
+        // while(n > 0) {
+        //     if((n&1) == 1) {
+        //         numberOfOne++;
+        //     }
+        //     n = n >> 1;
+        // }
+        // // core logic is 2^x will always have single set bit
+        // return numberOfOne == 1;
+
+        // Efficient
+        return n > 0 && (n & (--n)) == 0;
     }
 }

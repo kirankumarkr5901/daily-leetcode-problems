f7cfcf9f37d2b6e9610ca1cee528cd5385f2f347
Kirankumar K R <74645636+kirankumarkr5901@users.noreply.github.com>
2026-08-17 13:43:45 +0530

    Time: 233 ms (83.17%) | Memory: 47.4 MB (87.74%) - LeetSync

--- 1685-stone-game-v/stone-game-v.java
+++ 1685-stone-game-v/stone-game-v.java
@@ -1,6 +1,6 @@
 class Solution {
 
-    private int solve(int left, int right, int[] leftSum, int[][] dp) {
+    private int subProblem(int left, int right, int[] leftSum, int[][] dp) {
         if(left >= right) {
             return 0;
         }
@@ -12,11 +12,11 @@ class Solution {
             int leftPart = leftSum[mid] - ((left-1) >= 0 ? leftSum[left-1] : 0);
             int rightPart = leftSum[right] - leftSum[mid];
             if(leftPart < rightPart) {
-                score = Math.max(score, leftPart + solve(left, mid, leftSum, dp));
+                score = Math.max(score, leftPart + subProblem(left, mid, leftSum, dp));
             } else if(rightPart < leftPart) {
-                score = Math.max(score, rightPart + solve(mid+1, right, leftSum, dp));
+                score = Math.max(score, rightPart + subProblem(mid+1, right, leftSum, dp));
             } else {
-                score = Math.max(score, Math.max(leftPart + solve(left, mid, leftSum, dp), rightPart + solve(mid+1, right, leftSum, dp)));
+                score = Math.max(score, Math.max(leftPart + subProblem(left, mid, leftSum, dp), rightPart + subProblem(mid+1, right, leftSum, dp)));
             }
         }
         return dp[left][right] = score;
@@ -33,6 +33,6 @@ class Solution {
         for(int[] row: dp) {
             Arrays.fill(row, -1);
         }
-        return solve(0, n-1, leftSum, dp);
+        return subProblem(0, n-1, leftSum, dp);
     }
 }

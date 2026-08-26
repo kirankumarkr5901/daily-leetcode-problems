d9191adba3ca438ed761c7eba3764fa94d681889
Kirankumar K R <74645636+kirankumarkr5901@users.noreply.github.com>
2026-08-26 15:17:06 +0530

    Time: 17 ms (5.37%) | Memory: 47.3 MB (6.04%) - LeetSync

---
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -0,0 +1,46 @@
+class Solution {
+
+    private boolean compareStrings(String temp, String ans) {
+        // System.out.println("\nMismatch check "+temp+" -> "+ans);
+        if(temp.length() != ans.length()) return true;
+        // System.out.println("Len is equal");
+        for(int i = 0;i<temp.length();i++) {
+            if(temp.charAt(i) != ans.charAt(i)) {
+                // System.out.println("Mismatch caught at: "+i+" "+temp+" -> "+ans);
+                return temp.charAt(i) == '0';
+            }
+        }
+        return false;
+    }
+
+    public String shortestBeautifulSubstring(String s, int k) {
+        // BRUTE FORCE
+        int n = s.length();
+        if(n < k) {
+            return "";
+        }
+        for(int len = k;len<=n;len++) {
+            String ans = "";
+            for(int i = 0;i<= (n-len);i++) {
+                String temp = s.substring(i, i+len);
+                int count1 = 0;
+                for(char c: temp.toCharArray()) {
+                    if(c == '1') {
+                        count1++;
+                    }
+                }
+                if(count1 == k) {
+                    System.out.print("ans: "+ans+" temp: "+temp);
+                    if(ans.equals("") || compareStrings(temp, ans)) {
+                        ans = temp;
+                    }
+                    System.out.println(" -> "+ans);
+                }
+            }
+            if(!ans.equals("")) {
+                return ans;
+            }
+        }
+        return "";
+    }
+}

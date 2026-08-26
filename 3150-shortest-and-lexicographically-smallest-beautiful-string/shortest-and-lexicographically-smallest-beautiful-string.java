e61e235dd4210695533188c1663b7378334ea889
Kirankumar K R <74645636+kirankumarkr5901@users.noreply.github.com>
2026-08-26 15:18:03 +0530

    Time: 13 ms (5.37%) | Memory: 47.4 MB (6.04%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,18 +1,5 @@
 class Solution {
 
-    private boolean compareStrings(String temp, String ans) {
-        // System.out.println("\nMismatch check "+temp+" -> "+ans);
-        if(temp.length() != ans.length()) return true;
-        // System.out.println("Len is equal");
-        for(int i = 0;i<temp.length();i++) {
-            if(temp.charAt(i) != ans.charAt(i)) {
-                // System.out.println("Mismatch caught at: "+i+" "+temp+" -> "+ans);
-                return temp.charAt(i) == '0';
-            }
-        }
-        return false;
-    }
-
     public String shortestBeautifulSubstring(String s, int k) {
         // BRUTE FORCE
         int n = s.length();
